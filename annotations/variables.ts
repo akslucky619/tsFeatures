f2349c85309b668349abcadab1a32ab010300e07
akash@619 <Akash.sharma1@in.ey.com>
2021-02-02 23:35:37 +0530

    type annotations

---
+++ annotations/variables.ts
@@ -0,0 +1,25 @@
+let apples: number = 5;
+let speed: string = "fast";
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+// built in objects
+let now: Date = new Date();
+
+// arrays
+let colors: String[] = ["red", "blue", "green"];
+let myNums: number[] = [1, 2, 3];
+let truthys: boolean[] = [true, true, false];
+
+// classes
+class Car {}
+
+let car: Car = new Car();
+
+// objects
+let point: { x: number; y: number } = {
+  x: 20,
+  y: 20,
+};
