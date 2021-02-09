51a793b95b5975b1c50b5a063e3f66a380964401
akash@619 <Akash.sharma1@in.ey.com>
2021-02-09 22:42:34 +0530

    add more types

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -26,7 +26,24 @@ let point: { x: number; y: number } = {
 
 // function
 
-const logNumber: (i:number) => void = (i: number) => {
-console.log(i);
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
 
+// when to use annotaions
+// 1) Function that returns type any
+const json = '{"x":10, "y":20}';
+// const coordinates = JSON.parse(json); previous
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates);
+
+// 2) When we decare a variable on one line
+// and initialise it later
+let words = ["red", "blue", "green"];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === "green") {
+    foundWord = true;
+  }
 }
