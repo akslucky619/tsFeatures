112a8b2d7f2092d49dc7a3a54feef5abc60e2817
akash@619 <Akash.sharma1@in.ey.com>
2021-02-18 23:55:41 +0530

    add multiply

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -8,6 +8,11 @@ const substract = (a: number, b: number) => {
   a - b;
 };
 
+
 function divide(a: number, b: number): number {
   return a / b;
 }
+
+const multiply = function multiply(a: number, b: number): number {
+  return a * b;
+};
