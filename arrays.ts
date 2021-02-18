855d02eceab30cb35a9a48f8ec5fd593115284fa
akash@619 <Akash.sharma1@in.ey.com>
2021-02-19 02:27:52 +0530

    array annotations

---
+++ arrays.ts
@@ -0,0 +1,7 @@
+// const carMakers:string[] = []
+// when initialising an array
+const carMakers = ["ford", "mustang", "ferrari"];
+const dates = [new Date(), new Date()];
+
+const bikeMakers = [["yamaha"], ["honda"], ["kawasaki"]];
+// const bikeMakers:string[][] = [];
