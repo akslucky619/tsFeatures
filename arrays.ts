25b071b540c5d703a5aecc18de35327f70baa7c1
akash@619 <Akash.sharma1@in.ey.com>
2021-02-19 02:59:35 +0530

    array error

--- arrays.ts
+++ arrays.ts
@@ -5,3 +5,9 @@ const dates = [new Date(), new Date()];
 
 const bikeMakers = [["yamaha"], ["honda"], ["kawasaki"]];
 // const bikeMakers:string[][] = [];
+
+// help with inference when extracting values from an array
+const car1 = carMakers[0];
+
+// prevent incompatible values
+carMakers.push(100);
