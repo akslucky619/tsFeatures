a1f896a82a48b394ee0c1a01737d5f230dd93b27
akash@619 <Akash.sharma1@in.ey.com>
2021-02-18 23:38:28 +0530

    add divide

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -2,8 +2,12 @@ const add = (a: number, b: number): number => {
   return a + b;
 };
 
-// ts error when write annotations for return value 
+// ts error when write annotations for return value
 // i.e it will infer return as void
 const substract = (a: number, b: number) => {
- a - b;
+  a - b;
 };
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
