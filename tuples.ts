acc8d4842c670c37cab134b9343d25056bc545b6
akash@619 <Akash.sharma1@in.ey.com>
2021-02-23 22:27:41 +0530

    interfaces

--- tuples.ts
+++ tuples.ts
@@ -4,6 +4,9 @@ const drink = {
   sugar: 40,
 };
 
+// type alias
 type Drink = [string, boolean, number];
 
 const pepsi: Drink = ["brown", true, 50];
+const sprite: Drink = ["clear", true, 50];
+const tea: Drink = ["brown", false, 20];
