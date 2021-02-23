7be33df50ae64325fb4ab5670b34756ebf1889bd
akash@619 <Akash.sharma1@in.ey.com>
2021-02-24 00:20:54 +0530

    color

--- classes.ts
+++ classes.ts
@@ -1,8 +1,30 @@
 class Vehicle {
-  drive(): void {
-    console.log("chugga chugga");
+  color: string;
+  constructor(color: string) {
+    this.color = color;
+  }
+  protected honk(): void {
+    console.log("Honk honk");
   }
 }
 
-const vehicle = new Vehicle();
-vehicle.drive();
+const vehicle = new Vehicle("orange");
+console.log(vehicle.color);
+
+// class Car extends Vehicle {
+//   private drive(): void {
+//     console.log("vroom");
+//   }
+//   public startDrive(): void {
+//     this.drive();
+//     this.honk();
+//   }
+// }
+
+// // const vehicle = new Vehicle();
+// // vehicle.drive();
+// // vehicle.honk();
+
+// const car = new Car();
+// car.startDrive();
+// car.honk();
