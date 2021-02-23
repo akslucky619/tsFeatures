50a3ffbb053b745f6684a4065419cc6aec44a214
akash@619 <Akash.sharma1@in.ey.com>
2021-02-24 00:50:10 +0530

    add super

--- classes.ts
+++ classes.ts
@@ -10,20 +10,23 @@ class Vehicle {
 const vehicle = new Vehicle("orange");
 console.log(vehicle.color);
 
-// class Car extends Vehicle {
-//   private drive(): void {
-//     console.log("vroom");
-//   }
-//   public startDrive(): void {
-//     this.drive();
-//     this.honk();
-//   }
-// }
+class Car extends Vehicle {
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
+  private drive(): void {
+    console.log("vroom");
+  }
+  public startDrive(): void {
+    this.drive();
+    this.honk();
+  }
+}
 
-// // const vehicle = new Vehicle();
-// // vehicle.drive();
-// // vehicle.honk();
+// const vehicle = new Vehicle();
+// vehicle.drive();
+// vehicle.honk();
 
-// const car = new Car();
-// car.startDrive();
+const car = new Car(4, "red");
+car.startDrive();
 // car.honk();
